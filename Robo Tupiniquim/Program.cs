f61c92bd21a9b1e1cb6eb2c7446ec4b83c07bc0a
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-31 09:23:22 -0300

    Criação de variaveis X e Y e criação e um método para receber duas variaveis em uma linha

--- Robo Tupiniquim/Program.cs
+++ Robo Tupiniquim/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+
+
+            string[] limites = Console.ReadLine().Split(' ');
+            int xMax = int.Parse(limites[0]);
+            int yMax = int.Parse(limites[1]);
+
+            while (true)
+            {
+                string posicaoInicial = Console.ReadLine();
+                if (string.IsNullOrEmpty(posicaoInicial)) break;
+                string comandos = Console.ReadLine();
+
+            }
+            } } }
