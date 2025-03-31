ef4affb909bb0aad6c3b656b17c15b1e82cdd2cf
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-31 09:54:25 -0300

    Criação de métodos para virar a direita e virar a esquerda atraves de Switch case

--- Robo Tupiniquim/Program.cs
+++ Robo Tupiniquim/Program.cs
@@ -1,51 +1,66 @@
 ﻿
-
-public class NavegacaoRobo
+namespace Robo_Tupiniquim
 {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+    {
+        string[] limites = Console.ReadLine()!.Split(' ');
+        int xMax = int.Parse(limites[0]);
+        int yMax = int.Parse(limites[1]);
 
+        while (true)
         {
-            string[] limites = Console.ReadLine()!.Split(' ');
-            int xMax = int.Parse(limites[0]);
-            int yMax = int.Parse(limites[1]);
+            string posicaoInicial = Console.ReadLine()!;
+            string comandos = Console.ReadLine()!;
 
-            List<string> resultados = new List<string>();
-            while (true)
+            string[] posicao = posicaoInicial.Split(' ');
+            int x = int.Parse(posicao[0]);
+            int y = int.Parse(posicao[1]);
+            char direcao = char.Parse(posicao[2]);
+
+            foreach (char comando in comandos)
             {
-                string posicaoInicial = Console.ReadLine()!;
-                if (string.IsNullOrEmpty(posicaoInicial)) break;
-                string comandos = Console.ReadLine();
-
-                string[] posicao = posicaoInicial.Split(' ');
-                int x = int.Parse(posicao[0]);
-                int y = int.Parse(posicao[1]);
-                char direcao = char.Parse(posicao[2]);
-
-                foreach (char comando in comandos)
+                if (comando == 'E')
                 {
-                    if (comando == 'E')
-                    {
-                        direcao = VirarEsquerda(direcao);
-                    }
-                    else if (comando == 'D')
-                    {
-                        direcao = VirarDireita(direcao);
-                    }
-                    else if (comando == 'M')
-                    {
-                        Mover(ref x, ref y, direcao, xMax, yMax);
-                    }
+                    direcao = VirarEsquerda(direcao);
                 }
-                resultados.Add($"{x} {y} {direcao}");
+                else if (comando == 'D')
+                {
+                    direcao = VirarDireita(direcao);
+                }
+                else if (comando == 'M')
+                {
+                    Mover(ref x, ref y, direcao, xMax, yMax);
+                }
             }
-
-            foreach (string resultado in resultados)
-            {
-                Console.WriteLine(resultado);
-            }
+            Console.WriteLine($"{x} {y} {direcao}");
+            Console.ReadLine();
         }
-
-
     }
-}
+
+    public static char VirarEsquerda(char direcao)
+    {
+        switch (direcao)
+        {
+            case 'N': return 'O';
+            case 'O': return 'S';
+            case 'S': return 'L';
+            case 'L': return 'N';
+            default: return direcao;
+        }
+    }
+
+    public static char VirarDireita(char direcao)
+    {
+        switch (direcao)
+        {
+            case 'N': return 'L';
+            case 'L': return 'S';
+            case 'S': return 'O';
+            case 'O': return 'N';
+            default: return direcao;
+        }
+    }
+
+}}
