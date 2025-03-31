dff84923c45b9d83cea12e80909b34e90244e6ed
gabrielvsantoss <gabrielvs3131@gmail.com>
2025-03-31 09:27:46 -0300

    Criação de um foreach como comando principal do Robo

--- Robo Tupiniquim/Program.cs
+++ Robo Tupiniquim/Program.cs
@@ -5,7 +5,6 @@
         static void Main(string[] args)
         {
 
-
             string[] limites = Console.ReadLine().Split(' ');
             int xMax = int.Parse(limites[0]);
             int yMax = int.Parse(limites[1]);
@@ -16,5 +15,30 @@
                 if (string.IsNullOrEmpty(posicaoInicial)) break;
                 string comandos = Console.ReadLine();
 
+                string[] posicao = posicaoInicial.Split(' ');
+                int x = int.Parse(posicao[0]);
+                int y = int.Parse(posicao[1]);
+                char direcao = char.Parse(posicao[2]);
+
+                foreach (char comando in comandos)
+                {
+                    if (comando == 'E')
+                    {
+                        direcao = VirarEsquerda(direcao);
+                    }
+                    else if (comando == 'D')
+                    {
+                        direcao = VirarDireita(direcao);
+                    }
+                    else if (comando == 'M')
+                    {
+                        Mover(ref x, ref y, direcao, xMax, yMax);
+                    }
+                }
+                Console.WriteLine($"{x} {y} {direcao}");
             }
-            } } }
+        }
+
+       
+    }
+}
